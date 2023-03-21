4ee87465f27c3687cf60225cabd09525c957ed20
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-03-21 12:50:02 -0600

    Add files via upload

---
+++ EVA2_15_protected/src/eva2_15_protected/EVA2_15_protected.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_15_protected;
+
+import Prueba.Superclase;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_15_protected {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+class SubClase extends Superclase {
+private int Valor;
+    public void imprimirValor(){
+        System.out.println("el valor es: " + valor );
+    }
+}
+    
