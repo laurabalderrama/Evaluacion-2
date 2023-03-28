85120fa0bfba03ea46bd3b91b8bb931ea967c186
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-03-28 10:47:03 -0600

    Add files via upload

---
+++ EVA2_11_FIGURAS/src/eva2_11_figuras/Circulo.java
@@ -0,0 +1,36 @@
+package eva2_11_figuras;
+
+/**
+ * @author laura
+ */
+public class Circulo extends Figuras {
+    private double radio;
+    
+    public Circulo(){
+        radio = -1;
+    }
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
+    
+    @Override
+    public double calcularArea(){
+        double area = Math.PI*(radio*radio);
+        return area;
+    }
+    
+    @Override
+    public double calcularPerimetro(){
+        double peri = Math.PI*2*radio;
+        return peri;
+    }
+}
---
+++ EVA2_11_FIGURAS/src/eva2_11_figuras/EVA2_11_FIGURAS.java
@@ -0,0 +1,13 @@
+package eva2_11_figuras;
+
+/**
+ * @author laura
+ */
+public class EVA2_11_FIGURAS {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
