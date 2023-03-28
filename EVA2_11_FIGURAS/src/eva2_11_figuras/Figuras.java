e3748796d59e19436ce012b4511a00ebc2c8f58e
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-03-28 10:15:33 -0600

    Add files via upload

---
+++ EVA2_11_FIGURAS/src/eva2_11_figuras/Figuras.java
@@ -0,0 +1,13 @@
+package eva2_11_figuras;
+
+/**
+ * @author Diego
+ */
+public class Figuras {
+    public double calcularArea(){
+        return -1;
+    } 
+    public double calcularPerimetro(){
+        return -1;
+    } 
+}
