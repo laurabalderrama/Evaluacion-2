a086084201ba4b08dc047620cd4a512648ce9e7a
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-03-29 10:03:37 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,26 @@
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author laura
+ */
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        
+        //NO SE PUEDE HACER OBJETOS DE CLASES ABSTRACTAS
+        PruebaAbst abst = new Pruebaabst();
+    }
+}
+
+class Prueba{
+    
+}
+
+abstract class PruebaAbst{
+
+}
